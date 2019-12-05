1fbb4798b0fb9bf469fc36bb1a7a8dee99005401
최광호 <hibuz@hibuz.com>
2019-12-05 14:29:04 +0900

    Add todo ui

--- src/App.js
+++ src/App.js
@@ -2,10 +2,18 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import Counter from './components/Counter';
+import Todos from './components/Todos';
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <div>
+        <Counter number={0} />
+        <hr />
+        <Todos />
+        </div>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
